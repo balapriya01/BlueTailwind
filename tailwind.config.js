b26e0db2915829166acc192de49bc8b08b86a709
Baswal <balapriya1801@gmail.com>
2024-06-22 15:10:38 +0530

    BlueTailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    darkMode:'class',
+    theme:{
+        extend:{
+            colors:{
+                'logo':{
+                    200 : '#fed305',
+                    300 : '#fed569'
+
+                }
+            },
+            maxWidth: {
+                '330': '23rem',
+              }
+        }
+    }
+}
